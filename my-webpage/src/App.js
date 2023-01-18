ef298a4bf63339b69fc176cfac0fc40902d64e1b
Alex <98969983+DangerBirdStrikesAgain@users.noreply.github.com>
2023-01-18 14:10:13 +0000

    Socials Links
    Added links to my socials

--- my-webpage/src/App.js
+++ my-webpage/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from "react";
 
 function App() {
   return (
@@ -7,8 +8,15 @@ function App() {
         <section className = "Name">
           Alex Riddell-Webster
         </section>
-        <section className='Text'>
-          Hi there! My website is under construction and will be updated soon :)
+        <section className="Icons">
+          <a href="https://github.com/DangerBirdStrikesAgain"><img src={require("./github.png")} className="Icon" alt = {"GitHub"} /></a>
+          <a rel="me" href="https://infosec.exchange/@alexRW"><img src={require("./mastodon.png")} className="Icon" alt = {"Mastodon"} /></a>
+          <a href="https://twitter.com/AlexRW_"><img src={require("./twitter.png")} className="Icon" alt = {"Twitter"} /></a>        
+
+        </section>
+        <section className="Text">
+          I'm a Computer Scientist, rower and coffee enthusiast. <br></br>
+          My website is currently under construction and will be updated soon :)
         </section>
     </div>
   );
